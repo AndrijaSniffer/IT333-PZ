- Corrected BER calculation and changed method names to the convention.

BPSK.py
@@ -42,59 +42,59 @@ class BPSK:
         pass
 
     # Metoda instance za izračunavanje broja uzoraka po simbolu
-    def calculateNumberOfSamplesPerSymbol(self):
+    def calculate_number_of_samples_per_symbol(self):
         self.Ns = int(self.fs / self.baud)
 
     # Metoda instance za izračunavanje ukupnog broja uzoraka
-    def calculateNumberOfSamples(self):
+    def calculate_number_of_samples(self):
         self.N = self.nbits * self.Ns
 
     # Metoda instance za postavljanje vremena
-    def setUpTime(self):
+    def set_up_time(self):
         self.t = r_[0.0:self.N] / self.fs
 
     # Metoda instance za postavljanje vidljivog ograničenja u vremenskom domenu
-    def setUpVisibleLimit(self):
+    def set_up_visible_limit(self):
         self.time_domain_visible_limit = np.minimum(self.nbits / self.baud, self.symbols_to_show / self.baud)
 
     # Metoda instance za izračunavanje nosioca
-    def calculateCarrier(self):
+    def calculate_carrier(self):
         self.carrier_signal = cos(2 * pi * self.f0 * self.t)
 
     # Metoda instance za dodeljivanje ulaznih bitova
-    def assignInputBits(self):
+    def assign_input_bits(self):
         self.input_bits = np.random.randn(self.nbits, 1) > 0
 
-    def calculateNoise(self):
+    def make_noise(self):
         return self.noise_amplitude * np.random.normal(size=len(self.t))
 
     # Metoda instance za digitalno-analognu konverziju
-    def digitalToAnalogConvertor(self):
+    def digital_to_analog_convertor(self):
         self.input_signal = (np.tile(self.input_bits * 2 - 1, (1, self.Ns))).ravel()
         self.data_symbols = np.array([[GetBpskSymbol(self.input_bits[x])] for x in range(0, self.input_bits.size)])
 
-    def calculateBSPKSignalWithNoise(self):
-        self.calculateCarrier()
-        self.assignInputBits()
-        self.digitalToAnalogConvertor()
+    def make_bpsk_signal_with_noise(self):
+        self.calculate_carrier()
+        self.assign_input_bits()
+        self.digital_to_analog_convertor()
 
         # Generisanje Gausovog šuma
-        noise = self.calculateNoise()
+        noise = self.make_noise()
         input_signal_with_noise = self.input_signal + noise
 
         # Dodavanje šuma na BPSK signal
         self.bpsk_signal = input_signal_with_noise * self.carrier_signal
 
-    def calculateBER(self):
+    def calculate_ber(self):
         # Demodulacija BPSK signala
         demodulated_signal = self.bpsk_signal * self.carrier_signal
 
         # Filtriranje signala
         # Bolji BER se dobija bez filtriranja signala
-        # filtrirani_signal = np.convolve(demodulated_signal, np.ones(self.Ns) / self.Ns, mode='valid')
+        # filtered_signal = np.convolve(demodulated_signal, np.ones(self.Ns) / self.Ns, mode='valid')
 
         # Vizualizacija filtriranog signala
-        # plt.plot(self.t[:len(filtrirani_signal)], filtrirani_signal)
+        # plt.plot(self.t[:len(filtered_signal)], filtered_signal)
         # plt.show()
 
         # Računanje praga odlučivanja kao srednje vrednosti između min i max vrednosti filtriranog signala
@@ -115,14 +115,15 @@ class BPSK:
         # Računanje grešaka
         errors = 0
 
+        step = int(len(received_bits) / len(self.input_bits))
         it_start = 0
-        it_end = 39
+        it_end = 0 + step
         for input_bit in self.input_bits:
             # Upoređivanje sa ulaznim bitovima
             if not (input_bit != np.all(received_bits[it_start:it_end] == 0)):
                 errors += 1
-            it_start += 40
-            it_end += 40
+            it_start += step
+            it_end += step
 
         # Računanje BER
         if errors == 0:
@@ -132,9 +133,9 @@ class BPSK:
             return ber
 
         # Dodatni ispis
-        # print("Filtrirani Signal:", filtrirani_signal)
+        # print("Filtrirani Signal:", filtered_signal)
         # print("Prag Odlučivanja:", decision_threshold)
         # print("Primljeni Bitovi:", received_bits)
         # print("Ulazni Bitovi:", self.ulazni_bitovi)
-        # print("Min Filtrirani Signal:", np.min(filtrirani_signal))
-        # print("Max Filtrirani Signal:", np.max(filtrirani_signal))
+        # print("Min Filtrirani Signal:", np.min(filtered_signal))
+        # print("Max Filtrirani Signal:", np.max(filtered_signal))